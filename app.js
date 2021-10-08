e595d277c4d921ea2f29b2b42e8e167fb0c16640
Mohd Imran <mohd78696321@gmail.com>
2021-10-08 09:48:28 +0530

    feat: added list item

--- app.js
+++ app.js
@@ -4,7 +4,37 @@ const todoList = document.querySelector("#todo-list");
 
 function addTodo(event) {
   event.preventDefault();
-  console.log("clicked");
+
+  // creating a todo div
+  const todoDiv = document.createElement("div");
+  todoDiv.setAttribute("id", "todo");
+
+  // creating li
+  const newTodo = document.createElement("li");
+  newTodo.innerText = todoInput.value;
+  newTodo.setAttribute("id", "todo-item");
+
+  todoDiv.appendChild(newTodo);
+
+  // creating check mark button
+  const checkMarkButton = document.createElement("button");
+  checkMarkButton.setAttribute("id", "check-btn");
+  checkMarkButton.innerHTML = `<i class="fas fa-check"></i>`;
+
+  todoDiv.appendChild(checkMarkButton);
+
+  // creating trash button
+  const trashButton = document.createElement("button");
+  trashButton.setAttribute("id", "trash-btn");
+  trashButton.innerHTML = `<i class="fas fa-trash"></i>`;
+
+  todoDiv.appendChild(trashButton);
+
+  // append to list
+  todoList.appendChild(todoDiv);
+
+  // clearing todo input value
+  todoInput.value = "";
 }
 
 todoButton.addEventListener("click", addTodo);
