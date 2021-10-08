263063831464a3f0690dbdb6ed3bc175c072d224
Mohd Imran <mohd78696321@gmail.com>
2021-10-08 14:36:24 +0530

    feat: added filter option

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const todoInput = document.querySelector("#todo-input");
 const todoButton = document.querySelector("#todo-button");
 const todoList = document.querySelector("#todo-list");
+const filterOption = document.querySelector("#filter-todo");
 
 function addTodo(event) {
   event.preventDefault();
@@ -54,3 +55,31 @@ function deleteTodo(e) {
 }
 
 todoList.addEventListener("click", deleteTodo);
+
+function filterTodo(e) {
+  const todos = todoList.childNodes;
+
+  todos.forEach(function (todo) {
+    switch (e.target.value) {
+      case "all":
+        todo.style.display = "flex";
+        break;
+      case "completed":
+        if (todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+      case "uncompleted":
+        if (!todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+    }
+  });
+}
+
+filterOption.addEventListener("click", filterTodo);
